536ed01c7a7d0df3664abc6759d92df9aa4c926a
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-05-20 14:19:47 +0100

    More code cleanup

--- data/stages/angry.hx
+++ data/stages/angry.hx
@@ -1,23 +1,15 @@
-var shader:CustomShader = null;
 function create() {
 	splash.scale.x = 3;
 	splash.scale.y = 0.5;
-	fire.visible = false;
-	splash.visible = false;
-	rain.visible = false;
-	shader = new CustomShader("bloom");
-	camGame.addShader(shader);
+	fire.visible = splash.visible = rain.visible = false;
+	camGame.addShader(shader = new CustomShader("bloom"));
 	shader.Quality = 100.0;
 }
 
 function beatHit(curBeat) {
-	if (curBeat == 32) {
-		splash.visible = true;
-		rain.visible = true;
+	if (curBeat == 32 || curBeat == 460) {
+		splash.visible = !splash.visible;
+		rain.visible = !rain.visible
 	}
-	if (curBeat == 460) {
-		fire.visible = true;
-		splash.visible = false;
-		rain.visible = false;
-	}
+	if (curBeat == 460) fire.visible = true;
 }
